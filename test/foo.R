37921417291dee1e5638040f5917bade52e556cc
Gabriel Crépeault-Cauchon <gabrielcrepeault@gmail.com>
2018-09-19 22:13:02 -0400

    Ajout fonction test foo

---
+++ test/foo.R
@@ -0,0 +1,18 @@
+## Ceci est une fonction foo
+## Créé par Gabriel Crépeault-Cauchon
+## 19 septembre 2018
+
+foo <- function(x)
+{
+    if (x > 1)
+    {
+        print ('foo')
+    }
+    else
+    {
+        print(' Bye now.')
+    }
+}
+## Exemple
+foo(3)
+foo(0)
